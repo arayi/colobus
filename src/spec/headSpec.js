b98c3bae917e027c06c8c05cd7d7cc7717ee6b55
Melody Mustaine <mel.mustaine@gmail.com>
2016-09-28 12:17:41 -0700

    fix mysterious deleted close parenthesis

--- src/spec/headSpec.js
+++ src/spec/headSpec.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai'
 import head from '../functions/head'
 
-describe( 'head', () => {
+describe.only( 'head', () => {
   it('returns the first element of an array', () => {
     expect( head([1,2,3]) ).to.eql( 1 )
-  }
+  })
   it('returns the first element of a string', () => {
     expect( head('asdf') ).to.eql( 'a' )
   })
