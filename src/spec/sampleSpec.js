3e0eeacdc0bd41b904a7c24740d38dba1151062e
Serafin <leaf466@gmail.com>
2016-09-30 11:56:15 -0700

    fix the issue where sample would occasionally return out of bounds

--- src/spec/sampleSpec.js
+++ src/spec/sampleSpec.js
@@ -1,11 +1,17 @@
 import { expect } from 'chai'
 import sample from '../functions/sample'
 
+const NUMTESTS = 40
+
 describe( 'sample', () => {
   let testData = [1, 'pony', {a: "answer"}]
+  let sampleResult = sample(testData)
 
-  it('it samples to nearest integer (up)', () => {
-    expect( testData.indexOf(sample(testData)) !== -1 ).to.eql( true )
+  console.log( 'val: ' + testData.indexOf( sampleResult ) + '; result: ' + sampleResult + '; testData: ' + testData )
+
+  it('it returns a random element from an array', () => {
+    expect( testData.indexOf( sampleResult ) !== -1 ).to.eql( true )
   })
+
 
 })
